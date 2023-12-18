6253b47cf4ead6f75ee64c06b569e14c4c8c1e3d
rjornelas <rafael.j.ornelas@gmail.com>
2023-12-18 12:12:48 -0300

    Adding onSaveInstanceState to keep data even when the phone rotates

--- app/src/main/java/com/example/unit_converter/MainActivity.kt
+++ app/src/main/java/com/example/unit_converter/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.unit_converter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 import com.example.unit_converter.model.CalculationStrategyHolder
 import com.example.unit_converter.model.Calculator
 import com.example.unit_converter.model.strategies.CalculatorStrategy
@@ -18,6 +20,7 @@ import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var edtValue: EditText
     private lateinit var spConversions: Spinner
 
     private val supportedCalculatorStrategies = arrayOf(
@@ -32,14 +35,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setUi()
+        var value = 0.0
+        var position = 0
 
+        savedInstanceState?.let{
+            value = it.getDouble("VALUE")
+            position = it.getInt("POSITION")
+        }
+
+
+        initUi()
+        setUi(value, position)
         setActions()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putDouble("VALUE", edtValue.text.toString().toDouble())
+        outState.putInt("POSITION", spConversions.selectedItemPosition)
     }
 
     private fun setActions() {
         val btnConvert: Button = findViewById(R.id.btnConvert)
-        val edtValue: EditText =  findViewById(R.id.edtValue)
 
         btnConvert.setOnClickListener{
             try {
@@ -77,11 +94,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkPlural(result: Double) = result > 1
 
-    private fun setUi() {
-        spConversions = findViewById(R.id.spConversions)
+    private fun setUi(value: Double, position: Int) {
+
+        edtValue.setText(value.toString())
+
         val spAdapter = ArrayAdapter(this, R.layout.res_spinner_item, getSpinnerData())
         spAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
         spConversions.adapter = spAdapter
+        spConversions.setSelection(position)
+    }
+
+    private fun initUi() {
+        spConversions = findViewById(R.id.spConversions)
+        edtValue = findViewById(R.id.edtValue)
     }
 
     private fun getSpinnerData(): MutableList<String> {
